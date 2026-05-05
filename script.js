87ca8bd0818735cfed4c10ed28d8fd7df92fa638
Liv2334 <ohuard@ucsd.edu>
2026-05-05 15:53:17 -0700

    Add theme toggle functionality for dark mode

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
 
+toggle.addEventListener("click", () => {
+  // toggle dark mode
+  body.classList.toggle("dark_mode_body");
+
+  // toggle shape
+  toggle.classList.toggle("circle-shape");
+});
